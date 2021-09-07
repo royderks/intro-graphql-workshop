276fa5f522a405899a730631eb410166e218f901
royderks <10717410+royderks@users.noreply.github.com>
2021-09-07 15:25:55 +0200

    Add ex-5 solution

--- app/src/App.js
+++ app/src/App.js
@@ -1,57 +1,45 @@
-import { useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 import Product from './Product';
 
-export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetch('http://localhost:4000/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          query: `
-            query {
-              products {
-                id
-                title
-                thumbnail
-                price
-              }
-            }
-        `,
-        }),
-      });
-
-      const { data } = await result.json();
-
-      setProducts(data.products);
-      setLoading(false);
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      title
+      thumbnail
+      price
     }
+  }
+`;
 
-    fetchData();
-  }, [products.length]);
+export default function App() {
+  const { loading, data } = useQuery(GET_PRODUCTS);
 
   return (
-    <div class='container'>
-      <h2>Products</h2>
-      <div className='row'>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          products.map((product) => (
-            <Product
-              key={product.id}
-              title={product.title}
-              price={product.price}
-              thumbnail={product.thumbnail}
-            />
-          ))
-        )}
+    <>
+      <nav className='navbar navbar-light bg-light mb-4'>
+        <div className='container-fluid'>
+          <h2 className='navbar-brand'>My Shop</h2>
+          <span>🛒 &nbsp; Cart (0)</span>
+        </div>
+      </nav>
+      <div class='container'>
+        <h2>Products</h2>
+        <div className='row'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            data.products.map((product) => (
+              <Product
+                key={product.id}
+                title={product.title}
+                price={product.price}
+                thumbnail={product.thumbnail}
+              />
+            ))
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
